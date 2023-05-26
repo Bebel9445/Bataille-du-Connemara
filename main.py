379cf4fdbf78f6a91f0e50568827ff1b1444eafb
crips24 <92549357+crips24@users.noreply.github.com>
2023-05-26 20:28:35 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+import socket
+import sys
+import threading
+import time
+
+
+
+
+
 A = ["O","O","O","O","O","O","O","O","O","O"]
 B = ["O","O","O","O","O","O","O","O","O","O"]
 C = ["O","O","O","O","O","O","O","O","O","O"]
@@ -236,14 +245,92 @@ def tirer(x, y):
     except:
         print("Erreur !")
 
-porte_avion(input("Ordonnée du porte-avions --> "), int(input("Abscisse du porte-avions --> ")), input("Orientation (V/H) --> "))
+# porte_avion(input("Ordonnée du porte-avions --> "), int(input("Abscisse du porte-avions --> ")), input("Orientation (V/H) --> "))
 
-cuirasse(input("Ordonnée du cuirassé --> "), int(input("Abscisse du cuirassé --> ")), input("Orientation (V/H) --> "))
+# cuirasse(input("Ordonnée du cuirassé --> "), int(input("Abscisse du cuirassé --> ")), input("Orientation (V/H) --> "))
 
-croiseur(input("Ordonnée du croiseur --> "), int(input("Abscisse du croiseur --> ")), input("Orientation (V/H) --> "))
+# croiseur(input("Ordonnée du croiseur --> "), int(input("Abscisse du croiseur --> ")), input("Orientation (V/H) --> "))
 
-sous_marin(input("Ordonnée du sous-marin --> "), int(input("Abscisse du sous-marin --> ")), input("Orientation (V/H) --> "))
+# sous_marin(input("Ordonnée du sous-marin --> "), int(input("Abscisse du sous-marin --> ")), input("Orientation (V/H) --> "))
 
-destroyer(input("Ordonnée du destroyer --> "), int(input("Abscisse destroyer --> ")), input("Orientation (V/H) --> "))
+# destroyer(input("Ordonnée du destroyer --> "), int(input("Abscisse destroyer --> ")), input("Orientation (V/H) --> "))
 
-tirer(input("Ordonnée du tir --> "), int(input("Abscisse du tir --> ")))
+# tirer(input("Ordonnée du tir --> "), int(input("Abscisse du tir --> ")))
+
+
+
+
+
+def receive_messages(client_socket):
+    gotID=False
+    id=None
+    while True:
+        try:
+            data = client_socket.recv(1024)
+            if data:
+                if not gotID:
+                    id = data.decode()
+                    gotID=True
+                if data.decode()=="PPA":
+                    porte_avion(input("coordonée A-J"),int(input("coordonée 1-10")),input("sens : V-H"))
+
+
+            else:
+                # No data received, server has disconnected
+                client_socket.close()
+                break
+        except:
+            client_socket.close()
+            break
+
+
+def ping(socket):
+    while True :
+        time.sleep(1)
+        ping="ping"
+        socket.send(ping.encode())
+
+
+ip= input("entrez l'ip du serveur : ")
+port= input("entrez me port du serveur (vide pour défaut 10000) : ")
+
+if port=="":
+    port=10000
+
+try:
+    port=int(port)
+except: 
+    print("erreur a l'entrée du port(étape conversion string -> int )")
+    exit()
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = (ip, port)
+print(sys.stderr, 'connecting to %s port %s' % server_address)
+
+try:
+    sock.connect(server_address)
+
+    # Start a separate thread to receive messages from the server
+    # receive_thread = threading.Thread(target=receive_messages, args=(sock,))
+    # receive_thread.start()
+    # ping_thread = threading.Thread(target=ping, args=(sock,))
+    # ping_thread.start()
+    receive_messages(sock)
+
+
+
+
+
+
+
+    # while True:
+    #     message = input('Message: ')
+    #     if message == 'quit':
+    #         break
+    #     sock.send(message.encode())
+
+except ConnectionRefusedError:
+    print("Server is not available.")
+finally:
+    sock.close()
